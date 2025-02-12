26ee91eb48baa0635fca7b62407db167f7576dc6
mattelcock <matthew.elcock82@googlemail.com>
2025-02-12 08:59:41 +0000

    refactor: move auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import { Stack } from "expo-router";
 import "../global.css";
+import { Auth } from "@/components/Auth";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="login" />
-    </Stack>
+    <Auth>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="login" />
+      </Stack>
+    </Auth>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,16 @@
-import { Auth } from "@/components/Auth";
 import { Text, View } from "react-native";
 
 const App = () => {
   return (
-    <Auth>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text className="text-9xl">Logged in</Text>
-      </View>
-    </Auth>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-9xl">Logged in</Text>
+    </View>
   );
 };
 
